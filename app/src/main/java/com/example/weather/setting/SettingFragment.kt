57e796a9ad04a234d606fe72da544811717e89c3
thu21 <thutrinhvn21@gmail.com>
2021-05-30 00:11:57 +0700

    setting

--- app/src/main/java/com/example/weather/setting/SettingFragment.kt
+++ app/src/main/java/com/example/weather/setting/SettingFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.weather.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.weather.databinding.FragmentSettingBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,43 +16,51 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SettingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentSettingBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val binding: FragmentSettingBinding = DataBindingUtil.inflate<FragmentSettingBinding>(
+            inflater,
+            R.layout.fragment_setting,
+            container,
+            false
+        )
+
+        val togg = binding.toggleButtonGroupId
+        //val C=binding.CId
+
+        togg.addOnButtonCheckedListener { togg, checkedId, isChecked ->
+            if (isChecked) {
+               /* if (togg.checkedButtonId == R.id.CId) {
+                    showToast("C")
+                } else if (togg.checkedButtonId == R.id.FId) {
+                    showToast("F")
+                } else {
+                    showToast("K")
+                }*/
+                when (checkedId) {
+                    R.id.CId-> showToast("đã thay đổi")
+                    R.id.FId -> showToast("đã thay đổi")
+                    R.id.KId -> showToast("đã thay đổi")
+
+                }
+
+            } else {
+                if (togg.checkedButtonId == View.NO_ID) {
+                    showToast("Nothing")
                 }
             }
+        }
+
+        return binding.root
+    }
+
+    private fun showToast(str: String) {
+        Toast.makeText(this.context, str, Toast.LENGTH_SHORT).show()
     }
 }
