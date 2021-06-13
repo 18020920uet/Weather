e37418b05dce1e4391ce0991ad3005c162a87402
orange <inf.ng.nam@gmail.com>
2021-06-13 22:09:03 +0700

    Update Widget

--- app/src/main/java/com/example/weather/NewAppWidget.kt
+++ app/src/main/java/com/example/weather/NewAppWidget.kt
@@ -1,17 +1,11 @@
-package com.example.weatherapp
+package com.example.weather
 
-import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
-import android.appwidget.AppWidgetProviderInfo
 import android.content.Context
-import android.content.Intent
 import android.widget.RemoteViews
 import androidx.preference.PreferenceManager
-import com.example.weather.MainActivity
-import com.example.weather.R
 import com.example.weather.database.WeatherAppDatabase
-import com.example.weather.database.daos.LocationDatabaseDAO
 import com.example.weather.database.entities.Location
 import com.example.weather.network.OpenWeatherApi
 import com.example.weather.setting.Settings
@@ -26,8 +20,6 @@ class NewAppWidget : AppWidgetProvider() {
     var latitude: Double = 0.0
     var longitude: Double = 0.0
 
-    lateinit var widgetInfo: WidgetInfo
-
     private var widgetJob = Job()
     private var widgetScope = CoroutineScope(Dispatchers.Main + widgetJob)
 
@@ -36,21 +28,53 @@ class NewAppWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
-    override fun onEnabled(context: Context) {
-        val database = WeatherAppDatabase.getInstance(context).locationDatabaseDAO
+    private fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        val views = RemoteViews(context.packageName, R.layout.new_app_widget)
         widgetScope.launch {
+            val database = WeatherAppDatabase.getInstance(context).locationDatabaseDAO
             val location = database.getCurrentLocation()
             latitude = location!!.latitude
             longitude = location.longitude
             val settings = loadSettings(context)
-            widgetInfo = getCurrentLocationWeatherInformation(location)
-            widgetInfo.temperatureUnit = settings.temperatureUnit
+            val widgetInfo = getCurrentLocationWeatherInformation(location)
+            val temperatureText = getTemperatureText(
+                convertTemperature(widgetInfo.temperature.toDouble(), settings.temperatureUnit),
+                settings.temperatureUnit
+            )
+            val description = widgetInfo.description.capitalize()
+
+            views.setImageViewResource(
+                R.id.appwidget_icon,
+                when (widgetInfo.icon) {
+                    "mist_day" -> R.drawable.mist_day
+                    "day" -> R.drawable.day
+                    "cloudy_day" -> R.drawable.cloudy_day
+                    "thunderstorm" -> R.drawable.thunderstorm
+                    "rain" -> R.drawable.rain
+                    "snow" -> R.drawable.snow
+
+                    "sunset" -> R.drawable.sunset
+                    "sunrise" -> R.drawable.sunrise
+
+                    "mist_night" -> R.drawable.mist_night
+                    "cloudy_night" -> R.drawable.cloudy_night
+                    "night" -> R.drawable.night
+                    else -> R.drawable.day
+                }
+            )
+            views.setTextViewText(R.id.appwidget_city, widgetInfo.name.capitalize())
+            views.setTextViewText(R.id.appwidget_temp, temperatureText)
+            views.setTextViewText(R.id.appwidget_description, description)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
 
@@ -60,7 +84,7 @@ class NewAppWidget : AppWidgetProvider() {
                 location.latitude, location.longitude, "en"
             )
         val result = setUp.await()
-        var icon: String
+        val icon: String
         if (result.datetime < result.sys.sunrise || result.datetime > result.sys.sunset) {
             icon = when (result.weather[0].id) {
                 in 200..232 -> "thunderstorm"
@@ -86,20 +110,12 @@ class NewAppWidget : AppWidgetProvider() {
             name = location.locationName,
             temperature = result.main.temp.toInt(),
             description = result.weather[0].description,
-            temperatureUnit = TemperatureUnit.Celsius,
             icon = icon
         )
     }
 
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-
-
-
     data class WidgetInfo(
         var name: String,
-        var temperatureUnit: TemperatureUnit,
         var temperature: Int,
         var description: String,
         var icon: String
@@ -116,30 +132,5 @@ class NewAppWidget : AppWidgetProvider() {
             }
         return Settings(temperatureUnit)
     }
-
-
-}
-
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widget: NewAppWidget.WidgetInfo = appWidgetManager.getAppWidgetInfo(appWidgetId) as NewAppWidget.WidgetInfo
-
-    val widgetLocationText = context.getString(R.string.my_location_text)
-    val widgetTempText = context.getString(R.string.sample_temperature)
-    val widgetText = context.getString(R.string.appwidget_text)
-
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.new_app_widget)
-
-    views.setTextViewText(R.id.appwidget_city, widget.widgetInfo.name)
-    views.setTextViewText(R.id.appwidget_temp, widget.widgetInfo.temperature.toString())
-    views.setTextViewText(R.id.appwidget_description, widget.widgetInfo.description)
-//    views.setImageViewResource(R.id.weather_icon, widget.icon.)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
 }
 
